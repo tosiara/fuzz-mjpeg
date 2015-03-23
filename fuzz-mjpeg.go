d89657f0ef6b15d54be7424f848ce8ca2c9c75d2
saruhan <skarade@microsoft.com>
2015-03-23 12:09:54 -0700

    add support for picking server port

--- fuzz-mjpeg.go
+++ fuzz-mjpeg.go
@@ -1,45 +1,52 @@
 package main
+
 import (
 	"flag"
 	"fmt"
-	"strings"
+	"github.com/skarademir/naturalsort"
+	"io/ioutil"
 	"log"
 	"math"
 	"math/rand"
+	"net"
 	"net/http"
-	"io/ioutil"
 	"path/filepath"
-	"time"
 	"sort"
-	"github.com/skarademir/naturalsort"
+	"strconv"
+	"strings"
+	"time"
 )
+
 var ( // command line flag variables
 	folderpath string
-	boundary string
-	framerate int
-	fuzzmode bool
-) 
+	boundary   string
+	framerate  int
+	hostname   string
+	port       int
+	fuzzmode   bool
+)
 var ( // fuzz command line flag variables
 	fuzz_maxlength int64
 )
-func fuzzLength(length int) string{
+
+func fuzzLength(length int) string {
 	if rand.Intn(1) == 1 {
-		return fmt.Sprintf("%d",length)
+		return fmt.Sprintf("%d", length)
 	} else {
-		return fmt.Sprintf("%d",rand.Int63n(fuzz_maxlength))
+		return fmt.Sprintf("%d", rand.Int63n(fuzz_maxlength))
 	}
 }
 func fuzzBoundary() {
-	boundary = "A" + strings.Repeat("A",rand.Intn(16384)) //TODO Investigate if a larger string is any use?
+	boundary = "A" + strings.Repeat("A", rand.Intn(16384)) //TODO Investigate if a larger string is any use?
 }
 func fuzzFramerate() {
 	framerate = rand.Intn(359) + 1 //TODO Add a way to increase framerate range from 1-1/360
 }
 func handler(w http.ResponseWriter, r *http.Request) {
-//set header to multipart and describe the boundary name to be used elsewhere
-	w.Header().Set("Content-Type", "multipart/x-mixed-replace;boundary=" + boundary) //"multipart/x-mixed-replace;boundary=<boundary-name>") 
-	w.Header().Set("Connection:", "keep-alive") 
-	w.Header().Set("Transfer-Encoding", "chunked") 
+	//set header to multipart and describe the boundary name to be used elsewhere
+	w.Header().Set("Content-Type", "multipart/x-mixed-replace;boundary="+boundary) //"multipart/x-mixed-replace;boundary=<boundary-name>")
+	w.Header().Set("Connection:", "keep-alive")
+	w.Header().Set("Transfer-Encoding", "chunked")
 	//load file(s) from folderpath
 	files, _ := filepath.Glob(folderpath + "/*.jpeg")
 	sort.Sort(naturalsort.NaturalSort(files))
@@ -48,15 +55,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			panic(err)
 		}
-		var length string = fmt.Sprintf("%d",len(dat))
+		var length string = fmt.Sprintf("%d", len(dat))
 		if fuzzmode {
 			length = fuzzLength(len(dat))
 			fuzzFramerate()
 		}
 		w.Write([]byte("\n--" + boundary + "\nContent-Type: image/jpeg\nContent-length: " + length + "\r\n\r\n"))
 		w.Write(dat)
-		
-		time.Sleep(time.Second/time.Duration(framerate))
+
+		time.Sleep(time.Second / time.Duration(framerate))
 	}
 }
 func init() {
@@ -64,9 +71,11 @@ func init() {
 	flag.StringVar(&folderpath, "folderpath", "./1.mjpeg", "Location of jpeg files to be sent (in natural ascending order) to clients. Default: ./1.mjpeg/")
 	flag.StringVar(&boundary, "boundary", "<boundary-name>", "Name of the boundary used between frames. Default: '<boundary-name>'")
 	flag.IntVar(&framerate, "framerate", 10, "Framerate in frames per second. Default: 10")
-	flag.BoolVar(&fuzzmode,"fuzzmode", false, "Fuzzing Switch. If this is set, All params are ignored (except folderpath) Default: false")
+	flag.StringVar(&hostname, "hostname", "localhost", "Hostname. Default: 'localhost'")
+	flag.IntVar(&port, "port", 8080, "Serving port Default: 8080")
+	flag.BoolVar(&fuzzmode, "fuzzmode", false, "Fuzzing Switch. If this is set, All params are ignored (except folderpath) Default: false")
 	//define fuzzer command line flags
-	flag.Int64Var(&fuzz_maxlength,"fuzz_maxlength", math.MaxInt64, "Fuzzer Only: maximum reported frame length")
+	flag.Int64Var(&fuzz_maxlength, "fuzz_maxlength", math.MaxInt64, "Fuzzer Only: maximum reported frame length")
 	rand.Seed(42)
 }
 func main() {
@@ -75,5 +84,10 @@ func main() {
 		fuzzBoundary()
 	}
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	if port > 65535 || port < 0 {
+		fmt.Printf("bad port")
+		return
+	}
+	fulladdr := net.JoinHostPort(hostname, strconv.Itoa(port))
+	log.Fatal(http.ListenAndServe(fulladdr, nil))
 }
