093ab7ece1ee13c0464556578f81f2734f9a6f68
skarademir <saruhan@MacBook-Pro.local>
2015-03-25 00:29:38 -0700

    Added support for external fuzzed boundary headers

--- fuzz-mjpeg.go
+++ fuzz-mjpeg.go
@@ -1,45 +1,66 @@
 package main
+
 import (
 	"flag"
 	"fmt"
-	"strings"
+	"github.com/skarademir/naturalsort"
+	"io/ioutil"
 	"log"
 	"math"
 	"math/rand"
 	"net/http"
-	"io/ioutil"
 	"path/filepath"
-	"time"
 	"sort"
-	"github.com/skarademir/naturalsort"
+	"strings"
+	"time"
 )
+
 var ( // command line flag variables
 	folderpath string
-	boundary string
-	framerate int
-	fuzzmode bool
-) 
+	boundary   string
+	framerate  int
+	fuzzmode   bool
+)
 var ( // fuzz command line flag variables
 	fuzz_maxlength int64
+	fuzz_path      string
 )
-func fuzzLength(length int) string{
+var (
+	fuzzedHeader     string
+	fuzzedBoundaries []string
+)
+
+func fuzzLength(length int) string {
 	if rand.Intn(1) == 1 {
-		return fmt.Sprintf("%d",length)
+		return fmt.Sprintf("%d", length)
 	} else {
-		return fmt.Sprintf("%d",rand.Int63n(fuzz_maxlength))
+		return fmt.Sprintf("%d", rand.Int63n(fuzz_maxlength))
 	}
 }
 func fuzzBoundary() {
-	boundary = "A" + strings.Repeat("A",rand.Intn(16384)) //TODO Investigate if a larger string is any use?
+	boundary = "A" + strings.Repeat("A", rand.Intn(16384)) //TODO Investigate if a larger string is any use?
 }
 func fuzzFramerate() {
 	framerate = rand.Intn(359) + 1 //TODO Add a way to increase framerate range from 1-1/360
 }
+func getFuzzFiles() {
+	//get boundary files
+	boundaryFiles, _ := filepath.Glob(fuzz_path + "/*.boundary.txt")
+	for boundaryFile := range boundaryFiles {
+		dat, err := ioutil.ReadFile(boundaryFiles[boundaryFile])
+		if err != nil {
+			panic(err)
+		}
+		fuzzedBoundaries = append(fuzzedBoundaries, string(dat))
+	}
+}
 func handler(w http.ResponseWriter, r *http.Request) {
-//set header to multipart and describe the boundary name to be used elsewhere
-	w.Header().Set("Content-Type", "multipart/x-mixed-replace;boundary=" + boundary) //"multipart/x-mixed-replace;boundary=<boundary-name>") 
-	w.Header().Set("Connection:", "keep-alive") 
-	w.Header().Set("Transfer-Encoding", "chunked") 
+	//set header to multipart and describe the boundary name to be used elsewhere
+
+	w.Header().Set("Content-Type", "multipart/x-mixed-replace;boundary="+boundary) //"multipart/x-mixed-replace;boundary=<boundary-name>")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Transfer-Encoding", "chunked")
+
 	//load file(s) from folderpath
 	files, _ := filepath.Glob(folderpath + "/*.jpeg")
 	sort.Sort(naturalsort.NaturalSort(files))
@@ -48,15 +69,27 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			panic(err)
 		}
-		var length string = fmt.Sprintf("%d",len(dat))
+
+		w.Write([]byte("\n--" + boundary + "\n"))
+
+		var length string = fmt.Sprintf("%d", len(dat))
+
 		if fuzzmode {
-			length = fuzzLength(len(dat))
 			fuzzFramerate()
+			if len(fuzzedBoundaries) > 0 {
+				w.Write([]byte(fuzzedBoundaries[file%len(fuzzedBoundaries)]))
+			} else {
+				w.Write([]byte("Content-Type: image/jpeg\nContent-length: " + fuzzLength(len(dat))))
+			}
+
+		} else {
+			w.Write([]byte("Content-Type: image/jpeg\nContent-length: " + length))
 		}
-		w.Write([]byte("\n--" + boundary + "\nContent-Type: image/jpeg\nContent-length: " + length + "\r\n\r\n"))
+
+		w.Write([]byte("\r\n\r\n"))
 		w.Write(dat)
-		
-		time.Sleep(time.Second/time.Duration(framerate))
+
+		time.Sleep(time.Second / time.Duration(framerate))
 	}
 }
 func init() {
@@ -64,15 +97,17 @@ func init() {
 	flag.StringVar(&folderpath, "folderpath", "./1.mjpeg", "Location of jpeg files to be sent (in natural ascending order) to clients. Default: ./1.mjpeg/")
 	flag.StringVar(&boundary, "boundary", "<boundary-name>", "Name of the boundary used between frames. Default: '<boundary-name>'")
 	flag.IntVar(&framerate, "framerate", 10, "Framerate in frames per second. Default: 10")
-	flag.BoolVar(&fuzzmode,"fuzzmode", false, "Fuzzing Switch. If this is set, All params are ignored (except folderpath) Default: false")
+	flag.BoolVar(&fuzzmode, "fuzzmode", false, "Fuzzing Switch. If this is set, All params are ignored (except folderpath) Default: false")
 	//define fuzzer command line flags
-	flag.Int64Var(&fuzz_maxlength,"fuzz_maxlength", math.MaxInt64, "Fuzzer Only: maximum reported frame length")
+	flag.Int64Var(&fuzz_maxlength, "fuzz_maxlength", math.MaxInt64, "Fuzzer Only: maximum reported frame length")
+	flag.StringVar(&fuzz_path, "fuzz_path", "./1.mjpeg", "Location of fuzzed response.txt and response.txt files to be sent to clients. Default: ./1.mjpeg/")
 	rand.Seed(42)
 }
 func main() {
 	flag.Parse()
 	if fuzzmode {
 		fuzzBoundary()
+		getFuzzFiles()
 	}
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
